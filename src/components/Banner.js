47b9b9463aa518c3fee642cb4be5a9d4db71390b
cameronjtoy <70675026+cameronjtoy@users.noreply.github.com>
2023-01-15 17:34:31 -0500

    new skills

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -61,7 +61,7 @@ return (
                 <p>In my web development course, I have learned about the various technologies and frameworks used to build and maintain modern web applications. Through hands-on projects, I have gained experience in backend technologies such as servers, databases, and server-side programming languages like Python. I have also learned about front-end technologies like HTML, CSS, and JavaScript (React, Express), which are used to create user-friendly and responsive websites. </p>
                 <p>My studies in artificial intelligence have included a course in reinforcement learning, where I have learned about AI algorithms and their applications. This course has allowed me to work with real-world data and build models to make decisions and adapt to changing environments.</p>
                 <p>Overall, my coursework has given me a well-rounded education in computer science and has prepared me for a career as a software engineer. I am eager to apply my skills and knowledge to develop innovative and practical solutions for various problems.</p>
-                <button onClick={() => window.open("../assets/Cameron_ToyResume2022_.pdf", '_blank')}>
+                <button onClick={() => window.open("./../assets/Cameron_ToyResume2022_.pdf", '_blank')}>
                 Resume
                 <ArrowRightCircle size={25} />
                 </button>
